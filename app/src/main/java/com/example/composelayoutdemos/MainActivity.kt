990e646b91044ecb5bdebedba4bd25ca30857664
Yigit Ozgumus <yigitozgumus1@gmail.com>
2021-06-10 19:56:25 +0300

    first composable

--- app/src/main/java/com/example/composelayoutdemos/MainActivity.kt
+++ app/src/main/java/com/example/composelayoutdemos/MainActivity.kt
@@ -14,15 +14,16 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeLayoutDemosTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
+            ComposeLayoutDemoApp()
         }
     }
 }
+
+@Composable
+fun ComposeLayoutDemoApp() {
+
+}
+
 
 @Composable
 fun Greeting(name: String) {
