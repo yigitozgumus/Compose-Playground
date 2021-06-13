3cb7dd83a0faff3b60694b20dcd451091216f9d4
Yigit Ozgumus <yigitozgumus1@gmail.com>
2021-06-13 19:19:28 +0300

    Add Pegasus home screen navigation

---
+++ app/src/main/java/com/example/composelayoutdemos/navigation/topics/pegasus/PegasusResources.kt
@@ -0,0 +1,19 @@
+package com.example.composelayoutdemos.navigation.topics.pegasus
+
+import androidx.compose.ui.graphics.Color
+
+// Colors
+val buttonPrimary = Color(0xfffdb814)
+
+// Strings
+val searchFlight = "Uçuş Ara"
+val checkinFlight = "Check - in"
+val login = "Giriş Yap"
+val textBolbol = "BolBol Nedir?"
+val textSeat = "Koltuk"
+val textBaggage = "Bagaj"
+val textAdditionalService = "Ek Hizmetler"
+val textFlightAssistant = "Uçuş Asistanı"
+val textBarcodeScreen = "Biniş Kartları"
+val textCampaigns = "Kampanyalar"
+val textMore = "More"
